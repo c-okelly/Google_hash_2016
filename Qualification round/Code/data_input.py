f60968aa5feed7576637281d507a6e27dd04fdb2
c-okelly <okellyconor@gmail.com>
2016-02-12 11:20:52 +0000

    updated readme and start on data input file

--- Qualification round/Code/data_input.py
+++ Qualification round/Code/data_input.py
@@ -1,4 +1,41 @@
 ## File to take data input from file
 
+# Take file input and convert each item to ints and create list that holds each line a sepearte list.
+
+file_name = "busy_day.in"
+
+file = open(file_name, "r")
+
+file_object = file.read()
+
+file_split_line = file_object.splitlines()
+
+# for line in file_split_line:
+#     line.split()
+#     [int(i) for i in line]
+
+line_2 = file_split_line[2]
+print(line_2[0])
+
+## Take grid input
+
+senario_layout = file_split_line[0]
+
+## Number of products and weights
+
+no_products = file_split_line[1]
+
+product_weight = file_split_line[2]
 
 
+## Number of warehouse, locations and stocks
+
+
+no_warehouses = file_split_line[3]
+
+# start_wharehouse_range = no_warehouses + 1
+# end_wharehouse_range = start_wharehouse_range + (2 * no_warehouses)
+#
+# print(start_wharehouse_range,end_wharehouse_range)
+
+##
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Readme file for the Google Hash challange qualification round 2016. Original team was Silly Silicion.
 
-On attempting the problem was a group realised we did not have the required programming skill to complete the task.
+On attempting the problem was a group realised we did not have the required programming skill to complete the task in the time required.
 
 This is my attempt at trying to build a working implementation of the whole project with my own code and see if i can get it working.
 
